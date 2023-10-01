ef741b5a624e8bd94054a1e4ce8d3064451c2af9
Jobash Ali <jobashb12@gmail.com>
2023-10-01 23:40:56 +0500

    Birthday Wish

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="card">
+      <div class="back"></div>
+      <div class="front">
+        <div class="imgset">
+          <img
+            width="100%"
+            src="https://1.bp.blogspot.com/-Mgj9-rbs65E/XfMoPSD5gtI/AAAAAAAAURk/NBokE2gSS2cTSJ2em5lZ5hJDuTtRN7UVwCLcBGAsYHQ/s1600/2713997.png"
+          />
+        </div>
+      </div>
+      <div class="text-container">
+        <p id="head">Happy Birthday!</p>
+        <h5>From Mano</h5>
+        <p>
+          {/* I hope your special day will bring you lots of happiness, love, and
+          fun. You deserve them a lot. Enjoy! */}
+          TO A WONDERFUL GIRL, MAY YHIS DAY AND YOUR ENTIRE YEAR BRiNG YOU
+          CLOSER TO YOUR FRiENDS, FAMiLY, AND GOD. WITH ALL OF MY HEART, I AM
+          WISHING YOU A <br />
+          HAPPY BIRTHDAY!
+        </p>
+        <p>Hope your day goes great!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
